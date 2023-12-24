a1d6bbda7e23e16cc0f602c988147c225195372f
HadiChaaban <chaabanh@umich.edu>
2023-12-24 05:51:04 -0500

    Update uploading_utils.py

--- uploading_utils.py
+++ uploading_utils.py
@@ -4,8 +4,6 @@ from tiktok_uploader.upload import upload_videos
 from tiktok_uploader.auth import AuthBackend
 from selenium.webdriver.chrome.options import Options
 
-from youtube_uploader_selenium import YouTubeUploader
-
 
 from utils import *
 
@@ -28,12 +26,3 @@ def upload_to_tiktok():
     auth = AuthBackend(username=tiktok_username,
                        password=tiktok_password, cookies='cookies.txt')
     print(upload_videos(videos=videos, auth=auth, headless=True, options=options))
-
-
-def upload_to_youtube():
-    video_path = '123/rockets.flv'
-    metadata_path = '123/rockets_metadata.json'
-
-    uploader = YouTubeUploader(video_path, metadata_path)
-    was_video_uploaded, video_id = uploader.upload()
-    print("Video Uploaded!" if was_video_uploaded else "Video Failed to Upload.")
